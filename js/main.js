3b76763ee46c990ca58aaaabfe3e4d66422ca3a1
MisaelGC <gacrmu@hotmail.com>
2020-09-10 00:37:31 -0500

    Cambios en el slide

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 var p = {
         paginacion: document.querySelectorAll("#paginacion li"),
         item: 0,
-        cajaSlide: document.querySelector("#slide ul")
+        cajaSlide: document.querySelector("#slide ul"),
+        animacionSlide: "fade",
+        imgSlide: document.querySelectorAll("#slide ul li")
     }
     // Objecto con los metódos del slide
 
@@ -21,6 +23,20 @@ var m = {
 
     movimientoSlide: function(item) {
         p.cajaSlide.style.left = item * -100 + "%";
+        for (var i = 0; i < p.paginacion.length; i++) {
+            p.paginacion[i].style.opacity = .5;
+        }
+        p.paginacion[item].style.opacity = 1;
+        if (p.animacionSlide == "slide") {
+            p.cajaSlide.style.transition = "1s left ease-in-out";
+        }
+        if (p.animacionSlide == "fade") {
+            p.imgSlide[item].style.opacity = 0;
+            p.imgSlide[item].style.transition = "1s opacity ease-in-out";
+            setTimeout(function() {
+                p.imgSlide[item].style.opacity = 1;
+            }, 500);
+        }
     }
 }
 
