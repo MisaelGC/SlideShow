0da9e2132dcc1a299761f7ec7543bf833ed25756
MisaelGC <gacrmu@hotmail.com>
2020-09-10 00:56:04 -0500

    Cambio en el Slide en flechas laterales

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ var p = {
         paginacion: document.querySelectorAll("#paginacion li"),
         item: 0,
         cajaSlide: document.querySelector("#slide ul"),
-        animacionSlide: "fade",
-        imgSlide: document.querySelectorAll("#slide ul li")
+        animacionSlide: "slide",
+        imgSlide: document.querySelectorAll("#slide ul li"),
+        avanzar: document.querySelector("#slide #avanzar"),
+        retroceder: document.querySelector("#slide #retroceder")
     }
     // Objecto con los metódos del slide
 
@@ -14,10 +16,30 @@ var m = {
         for (var i = 0; i < p.paginacion.length; i++) {
             p.paginacion[i].addEventListener("click", m.paginacionSlide);
         }
+        p.avanzar.addEventListener("click", m.avanzar);
+        p.retroceder.addEventListener("click", m.retroceder);
     },
 
     paginacionSlide: function(item) {
         p.item = item.target.parentNode.getAttribute("item") - 1;
+        m.movimientoSlide(p.item);
+    },
+
+    avanzar: function() {
+        if (p.item == p.imgSlide.length - 1) {
+            p.item = 0;
+        } else {
+            p.item++;
+        }
+        m.movimientoSlide(p.item);
+    },
+
+    retroceder: function() {
+        if (p.item == 0) {
+            p.item = p.imgSlide.length - 1;
+        } else {
+            p.item--;
+        }
         m.movimientoSlide(p.item);
     },
 
@@ -30,7 +52,7 @@ var m = {
         if (p.animacionSlide == "slide") {
             p.cajaSlide.style.transition = "1s left ease-in-out";
         }
-        if (p.animacionSlide == "fade") {
+        if (p.animacionSlide == "slide") {
             p.imgSlide[item].style.opacity = 0;
             p.imgSlide[item].style.transition = "1s opacity ease-in-out";
             setTimeout(function() {
